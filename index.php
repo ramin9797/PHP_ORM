90e451953361e58d63ae3366a3ca738c662133b5
Ramin <ramin.hes.97@gmail.com>
2020-06-09 12:19:01 +0400

    php orm composer autoloader

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
 
-require_once("NamespaceAutoloader.php");
+require_once("vendor/autoload.php");
 use Router\Router;
 use Database\Database;
-
-
 use App\Models\Article;
 use App\Models\User;
-
-
-$object = new NamespaceAutoloader();
-$object->addNamespace("App","app");
-$object->addNamespace("Router","routes");
-$object->addNamespace("Database","database");
-$object->register();
-
 
 
 $url = $_GET['route']??'';
